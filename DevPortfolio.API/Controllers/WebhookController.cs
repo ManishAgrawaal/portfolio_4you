b51e5da60ca4733fe781b2b72e7ea5dae3a88df5
Manish Kumar <manishkumar.indup@gmail.com>
2026-08-24 14:05:27 +0530

    Add WebhookController

---
+++ DevPortfolio.API/Controllers/WebhookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace DevPortfolio.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WebhookController : ControllerBase
+    {
+        [HttpPost("resend")]
+        public async Task<IActionResult> ResendWebhook()
+        {
+            using var reader = new StreamReader(Request.Body);
+            var body = await reader.ReadToEndAsync();
+
+            Console.WriteLine("RESEND WEBHOOK RECEIVED:");
+            Console.WriteLine(body);
+
+            return Ok();
+        }
+    }
+}
